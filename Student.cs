4e94afbf65b564911423b28d3b987a61f9b81f88
Jarvi64 <129019096+Jarvi64@users.noreply.github.com>
2023-04-10 16:09:55 +0530

    Add files via upload

---
+++ Student.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoApi.Models;
+
+public class Student
+{
+    public int ID { get; set; }
+
+   
+    public string? Name { get; set; }
+
+   
+    public string? Class { get; set; }
+
+   
+    public bool HasPRN { get; set; }
+
+    public string? Gender { get; set; }
+
+
+}
